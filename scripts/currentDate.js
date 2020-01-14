4edc32413914f8980e5860b04f3a05ec37c8aa0d
nfspeed007 <isr15001@byui.edu>
2020-01-13 17:21:00 -0700

    First Commit

---
+++ scripts/currentDate.js
@@ -0,0 +1,5 @@
+// displays the current year
+document.getElementById('year').innerText = new Date().getFullYear();
+
+// displays when the page was last modified
+document.getElementById('lastModified').innerText = document.lastModified;
