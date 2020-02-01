c451e712300607d558fe90911dd26bcde80929da
nfspeed007 <isr15001@byui.edu>
2020-02-01 16:32:31 -0700

    Preston Page

---
+++ lesson-4/scripts/main.js
@@ -0,0 +1,38 @@
+
+// current date
+var cd = new Date();
+
+var daysOfWeek = [ 
+    'Sunday', 
+    'Monday', 
+    'Tuesday', 
+    'Wednesday', 
+    'Thursday', 
+    'Friday', 
+    'Saturday' ];
+
+var months = [
+    'January', 
+    'February', 
+    'March', 
+    'April', 
+    'May', 
+    'June', 
+    'July', 
+    "August", 
+    'September', 
+    'October', 
+    'November', 
+    'December'];
+
+// display full current date
+document.getElementById('currentDate').innerHTML = daysOfWeek[cd.getDay()] + ", " + cd.getDate() + " " + months[cd.getMonth()] + 
+" " + cd.getFullYear();
+
+// hamburger menu
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+    document.querySelector(".navigation").classList.toggle("responsive");
+}
