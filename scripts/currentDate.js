d6d3a88f070a14b724b51fb88beaa2593ed44c01
nfspeed007 <isr15001@byui.edu>
2020-02-13 09:58:59 -0700

    accidental file delete

--- scripts/currentDate.js
+++
@@ -1,5 +0,0 @@
-// displays the current year
-document.getElementById('year').innerHTML = new Date().getFullYear();
-
-// displays when the page was last modified
-document.getElementById('lastModified').innerHTML = document.lastModified;
